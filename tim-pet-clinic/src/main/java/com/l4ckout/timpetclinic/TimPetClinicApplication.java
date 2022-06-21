62bc4dadfe8178d28dd87a609ef0e6a3fc65a423
Timothy Tan <timothytan967>
2022-06-21 23:49:43 +0800

    Initial commit

---
+++ tim-pet-clinic/src/main/java/com/l4ckout/timpetclinic/TimPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.l4ckout.timpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimPetClinicApplication.class, args);
+	}
+
+}
---
+++ tim-pet-clinic/src/test/java/com/l4ckout/timpetclinic/TimPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l4ckout.timpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
